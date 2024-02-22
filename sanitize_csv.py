6e492f5d6676aaf87bfeb12687524ed0118c41b7
jmq <296806834@qq.com>
2024-02-22 11:42:24 +0800

    only support python3

--- sanitize_csv.py
+++ sanitize_csv.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python3
-from __future__ import print_function  # Allows print to be used as a function
-
+#!/usr/bin/python
 import csv
 import os
 import sys
-import io
-
 
 def escape_formulae(data):
     """Check if data starts with =,+,-,@ or contains tabs or carriage returns.
@@ -21,17 +17,17 @@ def remove_cmd_string(field):
 def process_csv(input_csv_filename, output_csv_filename):
     """Read a CSV file and apply data cleansing and escaping rules to each field.
     Then, write the sanitized data into a new CSV file."""
-    with io.open(input_csv_filename, 'r', encoding='utf-8') as csvfile:
+    with open(input_csv_filename, 'r', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
 
-        with io.open(output_csv_filename, 'wb') as outfile:
+        with open(output_csv_filename, 'w', newline='', encoding='utf-8') as outfile:
             writer = csv.writer(outfile, quotechar='"', quoting=csv.QUOTE_ALL)
 
             for row in reader:
                 sanitized_row = [
                     escape_formulae(remove_cmd_string(field)) for field in row
                 ]
-                writer.writerow([s.encode('utf-8') for s in sanitized_row])
+                writer.writerow(sanitized_row)
 
 def main(input_csv_filename):
     """Main function to check if the input CSV file exists and process it."""
@@ -44,6 +40,6 @@ def main(input_csv_filename):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python sanitize_csv.py <input_csv_filename>")
+        print("Usage: python script.py <input_csv_filename>")
     else:
         main(sys.argv[1])
